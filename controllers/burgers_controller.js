877a8dcec9c644bd93bd6c052512a8ca62056c60
joedan138 <joedan138@gmail.com>
2018-05-31 15:39:51 -0600

    working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,69 +3,40 @@ var express = require("express");
 var routes = express.Router();
 var burger = require("./../models/burger.js");
 
-routes.get("/", function (req, res) {
-    // get all of the cats data and render it to the index html file
-    burger.selectAll(function (data) {
-        var hbsObject = {
-            burgers:data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject)
-        
-    })
-});
 
-routes.get("/index", function (req, res) {
-    // get all of the cats data and render it to the index html file
-    burger.selectAll(function (data) {
-        var hbsObject = {
-            burgers:data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject)
-        
-    })
-});
+module.exports = function(app) {
+  app.get("/api/Reading", function(req, res) {
+    // 1. Add a join to include all of each Reading's Posts
+    db.Reading.findAll({}).then(function(readings) {
+      res.json(readings);
+    });
+  });
+  
+  app.get("/api/Reading/:mac", function(req, res) {
+    // 2; Add a join to include all of the Reading's Posts here
+    db.Reading.findAll({
+      where: {
+        mac: req.params.mac
+      }
+    }).then(function(readings) {
+      res.json(readings);
+    });
+  });
 
+  app.post("/api/Reading", function(req, res) {
+    db.Reading.create(req.body).then(function(readings) {
+      res.json(readings);
+    });
+  });
 
-routes.post("/api/burgers", function (req, res) {
-    console.log(req.body.name);
-    console.log(req.body.devoured);
-    
-    // ORM insertOne new burger with a name and whether or not is devoured
-    burger.insertOne(
-        "burgers",
-        "burger_name",
-        "devoured",
-        req.body.name,
-        req.body.devoured,
-        // 0,
-        function (result) {
-            // Send back the ID of the new quote
-            res.json({ id: result.insertId });
-        });
+  app.delete("/api/Reading/:mac", function(req, res) {
+    db.Reading.destroy({
+      where: {
+        mac: req.params.mac
+      }
+    }).then(function(readings) {
+      res.json(readings);
+    });
+  });
 
-});
-
-routes.put("/api/burgers/:id", function (req, res) {
-    // update a burgers "devoured" condition at its specific id
-    var condition = "id = " + req.params.id;
-    
-    console.log("condition: ", condition);
-
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function (result) {
-        if ((result.changedRows == 0)) {
-            return res.status(404).end();
-        } else {
-            // res.status(200).end
-            res.json(result);
-        }
-    }); 
-});
-
-
-module.exports = routes;
-
-// CHANGED EVER INSTANCE OF "router" TO "routes" WITH EXCEPTION - router initialization
+};
--- server.js
+++ server.js
@@ -1,35 +1,29 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// Sets up the Express App
-// =============================================================
+var PORT = process.env.PORT || 8080;
+
 var app = express();
-var PORT = process.env.PORT || 7000;
 
 // Requiring our models for syncing
-var db = require("models");
+var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // parse application/json
 app.use(bodyParser.json());
 
-// Static directory
-app.use(express.static(__dirname + "/public"));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-
-// Routes
-// =============================================================
-
+// Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
@@ -37,7 +31,7 @@ app.use(routes);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
   });
-});
